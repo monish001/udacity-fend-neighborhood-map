4c0c56f0557420168e753cfe473e2623172bab96
Monish Gupta <monish.gupta1@gmail.com>
2017-04-09 18:37:49 +0530

    partial chagnes

--- src/js/map-manager.js
+++ src/js/map-manager.js
@@ -1,7 +1,13 @@
 (function(document, globals){
     'use strict';
+    var RADIUS = '100';
     var ZOOM = 13;
     var INITIAL_POSITION = {lat: 40.7413549, lng: -73.9980244};
+    var LOCATIONS = [
+        // todo add more locations from Malerkotla as well
+        {title: 'Batra Studio', location: {lat: 30.3314833, lng: 76.38994}},
+        {title: `Expert Team Photography 'Wedding Cinema'`, location: {lat: 30.3314833, lng: 76.38994}}
+    ];
     var MapManager = function(){
         // currentPosition in format {lat: 40.7413549, lng: -73.9980244}
         this.currentPosition = INITIAL_POSITION;
@@ -21,11 +27,57 @@
     MapManager.prototype.getZoom = function(){
         return this.zoom;
     };
+    MapManager.prototype.createMarkers = function(places){
+        var bounds = new google.maps.LatLngBounds();
+        var placesList = document.getElementById('places');
+
+        for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+            };
+
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: image,
+                title: place.name,
+                position: place.geometry.location
+            });
+
+            placesList.innerHTML += '<li>' + place.name + '</li>';
+
+            bounds.extend(place.geometry.location);
+        }
+        this.map.fitBounds(bounds);
+    }
+    MapManager.prototype.updatePlaces = function(){
+        this.placesService.nearbySearch({
+            location: this.getCurrentPosition(),
+            // types: ['store'],
+            radius: RADIUS
+        }, function(results, status){
+            if (status == globals.google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    var place = results[i];
+                    console.log(place.geometry.location.lat());
+                    console.log(place.geometry.location.lng());
+                    createMarkers(results[i]);
+                }
+            }            
+        });
+    };
     MapManager.prototype.init = function(){
+        var self = this;
         this.map = new globals.google.maps.Map(document.getElementsByClassName('map-section')[0], {
-            center: this.getCurrentPosition(),
+            //center: PATIALA_LAT_LNG,
             zoom: this.getZoom()
         });
+        this.placesService = new google.maps.places.PlacesService(this.map);
+        this.updatePlaces();
+
         // var tribeca = {lat: 40.719, lng: -74.0089934};
         // var marker = new globals.google.maps.Marker({
         //     position: tribeca,
